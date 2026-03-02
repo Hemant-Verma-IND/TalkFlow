fc35d06c504c25b20866fe7a55814a0b86483663
Hemant Verma <152959300+Hemant-Verma-IND@users.noreply.github.com>
2026-03-03 03:38:57 +0530

    Fix: Add all missing backend dependencies

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 const allowedOrigins = process.env.HOSTLINK;
 app.use(securityHeaders);
 app.use(limiter);
-// app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+// app.use(cors({ origin: allowedOrigin, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
